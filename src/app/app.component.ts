819f561682e4aacbd60c058b54c6203d922964a0
Farah Alharastany <109542937+Farah-alharastany@users.noreply.github.com>
2024-10-13 23:27:40 +0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  columns: Array<{ field: string; header: string }> = [];
+  tasks: Array<{
+    id: any;
+    task_topic: string;
+    status: string;
+    priority: string;
+    created: Date;
+    end_date: Date;
+  }> = [];
+  selectedTasks: any;
+  constructor() {
+    this.columns = [
+      { field: 'task_topic', header: 'Task Topic' },
+      { field: 'status', header: 'Status' },
+      { field: 'priority', header: 'Priority' },
+      { field: 'created', header: 'Created' },
+      { field: 'end_date', header: 'End Date' },
+      { field: 'actions', header: 'Actions' },
+    ];
+    this.tasks = [
+      {
+        id:1,
+        task_topic: 'hello',
+        status: 'Completed',
+        priority: 'High',
+        created: new Date('05/14/2022'),
+        end_date: new Date('05/14/2022'),
+      },
+      {
+        id:2,
+        task_topic: 'hello',
+        status: 'Completed',
+        priority: 'High',
+        created: new Date('05/14/2022'),
+        end_date: new Date('05/14/2022'),
+      },
+    ];
+  }
   title = 'to-do-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TabViewModule } from 'primeng/tabview';
+import { TableModule } from 'primeng/table';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TabViewModule, TableModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
