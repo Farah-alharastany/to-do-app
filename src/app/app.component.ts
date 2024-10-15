ecdd959a668c2798ac5a66f0d42e8c9ca10a4d98
Farah Alharastany <109542937+Farah-alharastany@users.noreply.github.com>
2024-10-15 07:03:28 +0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  selected_status: String = '';
+  task_status_filter: Array<string> = [];
   columns: Array<{ field: string; header: string }> = [];
   tasks: Array<{
     id: any;
@@ -15,8 +17,21 @@ export class AppComponent {
     created: Date;
     end_date: Date;
   }> = [];
+  filtered_tasks: Array<{
+    id: any;
+    task_topic: string;
+    status: string;
+    priority: string;
+    created: Date;
+    end_date: Date;
+  }> = [];
   selectedTasks: any;
   constructor() {
+    // To store user selected status for filteration
+    this.selected_status = 'All Task';
+    // All tasks' status
+    this.task_status_filter = ['All Task', 'Pending', 'Completed'];
+    // Table columns
     this.columns = [
       { field: 'task_topic', header: 'Task Topic' },
       { field: 'status', header: 'Status' },
@@ -25,9 +40,10 @@ export class AppComponent {
       { field: 'end_date', header: 'End Date' },
       { field: 'actions', header: 'Actions' },
     ];
+    // Initlaize tasks array
     this.tasks = [
       {
-        id:1,
+        id: 1,
         task_topic: 'hello',
         status: 'Completed',
         priority: 'High',
@@ -35,14 +51,52 @@ export class AppComponent {
         end_date: new Date('05/14/2022'),
       },
       {
-        id:2,
+        id: 2,
         task_topic: 'hello',
-        status: 'Completed',
-        priority: 'High',
+        status: 'Pending',
+        priority: 'Medium',
+        created: new Date('05/14/2022'),
+        end_date: new Date('05/14/2022'),
+      },
+      {
+        id: 2,
+        task_topic: 'hello',
+        status: 'Pending',
+        priority: 'Low',
         created: new Date('05/14/2022'),
         end_date: new Date('05/14/2022'),
       },
     ];
+    // Initialize the filtered tasks array with the same data in the tasks array
+    this.filtered_tasks = this.tasks;
   }
-  title = 'to-do-app';
+  title = 'To Do App';
+
+  // To handle user click on the status in the filter part
+  handle_status_selection(status: String) {
+    this.selected_status = status;
+    this.filter_tasks();
+  }
+  // To update filtered tasks array
+  filter_tasks() {
+    if (this.selected_status === 'All Task') {
+      this.filtered_tasks = this.tasks;
+    } else {
+      this.filtered_tasks = this.tasks.filter(
+        (task) => task.status === this.selected_status
+      );
+    }
+  }
+
+  get_priorty_class(priority: String) {
+    if (priority === 'High') {
+      return 'high-priority';
+    } else if (priority === 'Medium') {
+      return 'medium-priority';
+    } else if (priority === 'Low') {
+      return 'low-priority';
+    } else {
+      return '';
+    }
+  }
 }
